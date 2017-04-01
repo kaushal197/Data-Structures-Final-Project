c2f4f391b60aeca3798651033dd1e056704d6a8b
Kaushal Patel <kaushal197@gmail.com>
2017-04-01 12:56:44 -0400

    Add files via upload

--- Galton-Board/script.js
+++ Galton-Board/script.js
@@ -1,7 +1,7 @@
-var frame = 480;
+var frame = 240;
 var bins = 5;
 var board = Array.apply(null, Array(bins)).map(Number.prototype.valueOf,0);
-var n = 1000000000;
+var n = 500;
 var start_n = n;
 var fix_val = 4/3;
 var fill_size = 10;
@@ -9,47 +9,71 @@ var pause = false;
 
 function setup() {
   createCanvas(frame*(4/3), frame);
+
+  var params = getURLParams();
+  n = params.n;
+  start_n = n;
+
   background("#333");
   noStroke();
-  grid();
+
   if(n>100000){
-    fill_size = 1/250;
     bins = bins*3;
     board = Array.apply(null, Array(bins)).map(Number.prototype.valueOf,0);
   }else if(n>10000){
-    fill_size = 1/50;
     bins = bins*3;
     board = Array.apply(null, Array(bins)).map(Number.prototype.valueOf,0);
   }
-  textSize(30);
+
+  textSize(frame/20);
   textAlign(CENTER);
   frameRate(60);
 }
 
 function draw() {
   background("#333");
-  grid();
-  text(n + " " + (round(int(frameRate())/10))*10, width/2, height/2);
-  text(int(Math.max.apply(null, board)*(1/fill_size)), width/2, height/1.5);
+  fill("#ffb76e");
+  text(n + " " + (round(int(frameRate())/10))*10, width/2, height/4);
+  text("Max Value: " + int(Math.max.apply(null, board)*(1/fill_size)) + "/" + start_n, width/2, height/3);
 
-  for(var i = 0; (n > 0) && (i <= start_n/(log(start_n)**2)); i++){
+  /*for(var i = 0; (n > 0) && (i <= start_n/(log(start_n)**2)); i++){
     if(!pause){
       place();
       n--;
     }
+  }*/
+
+  if(!pause){
+    console.time("place");
+    for(var i = 0; n > 0; i++){
+      place();
+      n--;
+    }
+    console.timeEnd("place");
   }
+
+
+  while(Math.max.apply(null, board) >= height*0.6){
+    fill_size = fill_size/fix_val;
+    board.forEach(normalize);
+  }
+
+  grid();
+  pause = true;
 }
 
 function grid(){
+  //Draws the histogram
   for(var i = 0; i < bins+1; i++) {
-    //rect(i*(width/bins)+4, 0, 4, height);
-    fill("#2897B7");
-    rect(i*(width/bins)+8, height-board[i], (width/bins)-2, board[i]);
-    fill("#fff");
+    fill("#86e3ff");
+    stroke("#fff");
+    strokeWeight(4);
+    rect(i*(width/bins), height-board[i], (width/bins), board[i]);
+    noStroke();
   }
 }
 
-// n number of balls
+// n number of balls to be dropped
 function place(){
 
   var j = 0;
@@ -60,18 +84,16 @@ function place(){
     j++;
   }
 
-  round(loc);
-  board[loc] += fill_size;
-  //console.log(board[loc]);
-
-  if(Math.max.apply(null, board) >= height*0.6){
-    fill_size = fill_size/fix_val;
-    //console.log(board);
-    board.forEach(normalize);
-    //console.log(board);
+  if((loc*10)%10 >= 5){
+    loc = int(loc) + 1;
+  } else {
+    loc = int(loc);
   }
+
+  board[loc] += fill_size;
 }
 
+//Fixes vertical sizing of each bin
 function normalize(item, index){
   board[index] = item/fix_val;
 }
